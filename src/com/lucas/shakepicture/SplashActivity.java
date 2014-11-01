1ba9984f469875e88c4ba2fcb8cb82745a8ab1af
LucasJia <jiayangcg@hotmail.com>
2014-11-01 20:52:17 +0800

    splash界面的睡眠时间改为1s

--- src/com/lucas/shakepicture/SplashActivity.java
+++ src/com/lucas/shakepicture/SplashActivity.java
@@ -93,7 +93,7 @@ public class SplashActivity extends Activity {
 //                        }
 //                    }
                     
-                    Thread.sleep(1);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
